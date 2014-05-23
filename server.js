47babed62c62a52a1a497f08f2ed4024e8e9e556
Dmitry Maslov <maslov.dmitrij@gmail.com>
2014-05-23 12:45:04 +0300

    enabled gzip compression

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.configure(function(){
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(express.compress());
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
